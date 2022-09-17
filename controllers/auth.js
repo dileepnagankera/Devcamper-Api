3629251671209214809bee0f2295d46dff78736e
Dileep Reddy <dileepnagankera@gmail.com>
2022-09-17 14:27:33 +0530

    User Login

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,9 +3,8 @@ const asyncHandler = require("../middleware/async");
 const User = require("../models/User");
 
 //@desc   Register user
-//@route  GET/api/v1/auth/register
+//@route  POST/api/v1/auth/register
 //@access  Public
-
 exports.register = asyncHandler(async (req, res, next) => {
   const { name, email, password, role } = req.body;
 
@@ -16,6 +15,37 @@ exports.register = asyncHandler(async (req, res, next) => {
     password,
     role,
   });
+
+  //Create Token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
+});
+
+//@desc   Login user
+//@route  POST/api/v1/auth/login
+//@access  Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //validate email & password
+  if (!email || !password) {
+    return next(new ErrorResponse("Please provide an email and password", 400));
+  }
+
+  //Check for user
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorResponse("Invalid credential", 401));
+  }
+
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
 
   //Create Token
   const token = user.getSignedJwtToken();
