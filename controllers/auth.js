82ab7a8812c0a31050e05c8f40532e64621c8340
Dileep Reddy <dileepnagankera@gmail.com>
2022-09-17 15:15:52 +0530

    Sending JWT In a Cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,10 +16,7 @@ exports.register = asyncHandler(async (req, res, next) => {
     role,
   });
 
-  //Create Token
-  const token = user.getSignedJwtToken();
-
-  res.status(200).json({ success: true, token });
+  sendTokenRespones(user, 200, res);
 });
 
 //@desc   Login user
@@ -48,7 +45,26 @@ exports.login = asyncHandler(async (req, res, next) => {
   }
 
   //Create Token
-  const token = user.getSignedJwtToken();
-
-  res.status(200).json({ success: true, token });
+  sendTokenRespones(user, 200, res);
 });
+
+//Get token from model, create cookie and send respones
+const sendTokenRespones = (user, statusCode, res) => {
+  //Create token
+  const token = user.getSignedJwtToken();
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === "production") {
+    options.secure = true;
+  }
+
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    token,
+  });
+};
