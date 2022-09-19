5097f45b57bbcd55436bcd6a134ff405999ea516
Dileep Reddy <dileepnagankera@gmail.com>
2022-09-19 11:24:10 +0530

    Bootcamp  User Relationship

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,9 @@
 const jwt = require("jsonwebtoken");
 const asyncHandler = require("./async");
-const ErrorRespones = require("../utils/errorResponse");
+const ErrorResponse = require("../utils/errorResponse");
 const User = require("../models/User");
 
-//Protect routes
+// Protect routes
 exports.protect = asyncHandler(async (req, res, next) => {
   let token;
 
@@ -11,37 +11,37 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
+    // Set token from Bearer token in header
     token = req.headers.authorization.split(" ")[1];
+    // Set token from cookie
   }
   // else if (req.cookies.token) {
-  //token = req.cookies.token;
-  //}
+  //   token = req.cookies.token;
+  // }
 
-  //Make sure token exists
+  // Make sure token exists
   if (!token) {
-    return next(new ErrorRespones("Not authorize to access this route", 401));
+    return next(new ErrorResponse("Not authorized to access this route", 401));
   }
 
-  //Verify token
   try {
+    // Verify token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    console.log(decoded);
 
     req.user = await User.findById(decoded.id);
 
     next();
   } catch (err) {
-    return next(new ErrorRespones("Not authorize to access this route", 401));
+    return next(new ErrorResponse("Not authorized to access this route", 401));
   }
 });
 
-// Grant access to apecific roles
+// Grant access to specific roles
 exports.authorize = (...roles) => {
   return (req, res, next) => {
-    if (!roles.includes(req.user.roles)) {
+    if (!roles.includes(req.user.role)) {
       return next(
-        new ErrorRespones(
+        new ErrorResponse(
           `User role ${req.user.role} is not authorized to access this route`,
           403
         )
